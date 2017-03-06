fe00634faa28dbb22612010b71be9fe36b89cd68
lzq2006 <lizhengqiu2006@126.com>
2017-03-06 09:43:22 +0800

    3.6test修改了main哦

--- app/src/main/java/com/example/jp/kaiyan_lzq01/MainActivity.java
+++ app/src/main/java/com/example/jp/kaiyan_lzq01/MainActivity.java
@@ -13,7 +13,7 @@ import com.example.jp.kaiyan_lzq01.faxian.FaXianFragment;
 import com.example.jp.kaiyan_lzq01.guanzhu.GuanZhuFragment;
 import com.example.jp.kaiyan_lzq01.jingxuan.JingXuanFragment;
 import com.example.jp.kaiyan_lzq01.wode.WoDeFragment;
-/*testvvv tesp*/
+/*testvvv ttttesp*/
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     private RadioGroup rgBottom;
